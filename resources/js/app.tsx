698e01389abc6f1b030c1ebe71b7482545324679
Vitaliy.Holovka <alfared3006@gmail.com>
2026-09-24 12:36:49 +0200

    CRM-ERP15: Implement Kubernetes infrastructure with Docker and Nginx setup, add production configurations, and update frontend build process

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,34 +1,31 @@
-import '../css/app.css';
+import "../css/app.css";
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import { createRoot } from "react-dom/client";
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => {
-        const pages = import.meta.glob(
-            './Pages/**/*.{jsx,tsx}'
-        )
 
-        return resolvePageComponent(
-            `./Pages/${name}.tsx`,
-            pages
-        ).catch(() =>
-            resolvePageComponent(
-                `./Pages/${name}.jsx`,
-                pages
-            )
-        )
+    resolve: (name) => {
+        const pages = import.meta.glob([
+            "./Pages/**/*.tsx",
+            "!./Pages/**/*.test.tsx",
+            "!./Pages/**/*.spec.tsx",
+        ]);
+
+        return resolvePageComponent(`./Pages/${name}.tsx`, pages);
     },
+
     setup({ el, App, props }) {
         const root = createRoot(el);
 
         root.render(<App {...props} />);
     },
+
     progress: {
-        color: '#4B5563',
+        color: "#4B5563",
     },
 });
